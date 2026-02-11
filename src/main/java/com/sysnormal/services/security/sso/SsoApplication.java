94ae443f2686de35f4868f473359b057c7b5c280
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2026-02-11 15:23:14 -0300

    removend commentS

--- src/main/java/com/sysnormal/services/security/sso/SsoApplication.java
+++ src/main/java/com/sysnormal/services/security/sso/SsoApplication.java
@@ -6,21 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SsoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SsoApplication.class, args);
 	}
-
-	@PostConstruct
-	public void testClassLoader() {
-		try {
-			Class.forName(
-					"com.sysnormal.starters.security.sso.sso_starter.database.migrations.V1__CreateAllTablesAndConstraints"
-			);
-			System.out.println("xxxxxxxxxxxxx Migration class FOUND");
-		} catch (ClassNotFoundException e) {
-			System.out.println("zzzzzzzzzzzzzzz Migration class NOT FOUND");
-		}
-	}
-
 }
