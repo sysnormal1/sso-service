5a6882937b54c8b22ff53345acf68d06d14a48c0
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2026-03-21 10:27:17 -0300

    migrated dependencies to maven central

--- src/main/java/com/sysnormal/services/security/sso/SsoApplication.java
+++ src/main/java/com/sysnormal/services/security/sso/SsoApplication.java
@@ -1,6 +1,5 @@
 package com.sysnormal.services.security.sso;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
